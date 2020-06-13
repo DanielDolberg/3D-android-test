6b4bcea4309d086ff707022c270c30f88431c579
DanielDolberg <Danix1200>
2020-06-13 17:53:43 +0300

    Triangle test

--- app/src/main/java/com/TheGingerMan/spaceobjects/MainView.java
+++ app/src/main/java/com/TheGingerMan/spaceobjects/MainView.java
@@ -16,6 +16,11 @@ public class MainView extends View implements Runnable{
     Edge[] lineEdges;
     SpacialObject line;
 
+    Vertex[] triVertices;
+    Edge[] triEdges;
+    SpacialObject tri;
+
+
     Thread runthread;
 
 
@@ -28,33 +33,33 @@ public class MainView extends View implements Runnable{
 
         space = new Space(width,height);
 
-
+/////////////////////////////////////////////////////////////////
         lineVertices = new Vertex[2];
         lineEdges = new Edge[1];
 
-        lineVertices[0] = new Vertex(4,-4,0);
-        lineVertices[1] = new Vertex(4,-4,0);
+        lineVertices[0] = new Vertex(width/320,-width/320,0);
+        lineVertices[1] = new Vertex(width/320,-width/320,0);
 
         lineEdges[0] = new Edge(lineVertices[0], lineVertices[1]);
 
         line = new SpacialObject(new Pivot(new Vector(0,0,0)));
         line.vertices = lineVertices;
         line.edges = lineEdges;
-
+/////////////////////////////////////////////////////////////////////////////////////////
         cubeVertices = new Vertex[8];
         cubeEdges = new Edge[12];
 
 
 
-        cubeVertices[0] = new Vertex(-5,5,1);// /\----------
-        cubeVertices[1] = new Vertex(5,5,1); // ----------/\
-        cubeVertices[2] = new Vertex(-5,-5,1);// \/----------
-        cubeVertices[3] = new Vertex(5,-5,1);// ----------\/
+        cubeVertices[0] = new Vertex(-width/256,width/256,width/1280);// /\----------
+        cubeVertices[1] = new Vertex(width/256,width/256,width/1280); // ----------/\
+        cubeVertices[2] = new Vertex(-width/256,-width/256,width/1280);// \/----------
+        cubeVertices[3] = new Vertex(width/256,-width/256,width/1280);// ----------\/
 
-        cubeVertices[4] = new Vertex(-5,5,-1);// /\----------
-        cubeVertices[5] = new Vertex(5,5,-1); // ----------/\
-        cubeVertices[6] = new Vertex(-5,-5,-1);// \/----------
-        cubeVertices[7] = new Vertex(5,-5,-1);// ----------\/
+        cubeVertices[4] = new Vertex(-width/256,width/256,-width/1280);// /\----------
+        cubeVertices[5] = new Vertex(width/256,width/256,-width/1280); // ----------/\
+        cubeVertices[6] = new Vertex(-width/256,-width/256,-width/1280);// \/----------
+        cubeVertices[7] = new Vertex(width/256,-width/256,-width/1280);// ----------\/
 
         cubeEdges[0] = new Edge(cubeVertices[0],cubeVertices[1]);
         cubeEdges[1] = new Edge(cubeVertices[0],cubeVertices[2]);
@@ -75,7 +80,30 @@ public class MainView extends View implements Runnable{
         cube.vertices = cubeVertices;
         cube.edges = cubeEdges;
 
+        ///////////////////////////////////////////////////////////////////////////
 
+        triVertices = new Vertex[5];
+        triVertices[0] = new Vertex(0,-width/256,0);
+        triVertices[1] = new Vertex(-width/640,width/640,width/640);
+        triVertices[2] = new Vertex(-width/640,width/640,-width/640);
+        triVertices[3] = new Vertex(width/640,width/640,-width/640);
+        triVertices[4] = new Vertex(width/640,width/640,width/640);
+
+        triEdges = new Edge[8];
+        triEdges[0] = new Edge(triVertices[1],triVertices[0]);
+        triEdges[1] = new Edge(triVertices[2],triVertices[0]);
+        triEdges[2] = new Edge(triVertices[3],triVertices[0]);
+        triEdges[3] = new Edge(triVertices[4],triVertices[0]);
+        triEdges[4] = new Edge(triVertices[1],triVertices[2]);
+        triEdges[5] = new Edge(triVertices[2],triVertices[3]);
+        triEdges[6] = new Edge(triVertices[3],triVertices[4]);
+        triEdges[7] = new Edge(triVertices[1],triVertices[4]);
+
+        tri = new SpacialObject(new Pivot(new Vector(0,0,0)));
+        tri.vertices = triVertices;
+        tri.edges = triEdges;
+
+        ///////////////////////////////////////////////////////////////////////////
         runthread = new Thread(this, "runthread");
         runthread.start();
     }
@@ -86,7 +114,8 @@ public class MainView extends View implements Runnable{
 
         space.draw(canvas);
         //line.draw(canvas, space);
-        cube.draw(canvas,space);
+        //cube.draw(canvas,space);
+        tri.draw(canvas,space);
 
         invalidate();
     }
@@ -96,9 +125,9 @@ public class MainView extends View implements Runnable{
     public void run() {
         while (true) {
 
-            cube.rotateGlobalX(0.1f);
-            cube.rotateGlobalY(0.1f);
-            cube.rotateGlobalZ(0.1f);
+           tri.rotateGlobalX(0.1f);
+           tri.rotateGlobalY(0.1f);
+           tri.rotateGlobalZ(0.1f);
 
                 try {
                     Thread.sleep(1);
